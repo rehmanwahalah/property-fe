58d02a567214b7b7b1e3bd962110025836201148
Abdul Rehman <abdul.rehman@creasoft.io>
2024-09-07 18:53:37 +0100

    quick fix

--- src/app/card-detail/page.js
+++ src/app/card-detail/page.js
@@ -1,46 +1,28 @@
 "use client";
 import React, { Suspense, useEffect, useState } from "react";
-import classes from "./cardDetail.module.css";
-import SliderPro from "../components/Slider";
-import { useSearchParams } from "next/navigation";
-import { propertyService } from "@/services/property.service";
+import CardDetailComponent from "../components/Card/detail";
 
 const CardDetail = () => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id"); // Get the property ID from the query string
-  const [property, setProperty] = useState({});
+  // const searchParams = useSearchParams();
+  // const id = searchParams.get("id"); // Get the property ID from the query string
+  // const [property, setProperty] = useState({});
 
-  const fetchPropertDetails = async (id) => {
-    try {
-      const resp = await propertyService.getPropertyDetail(id);
+  // const fetchPropertDetails = async (id) => {
+  //   try {
+  //     const resp = await propertyService.getPropertyDetail(id);
 
-      if (resp.status === 200) setProperty(resp.data.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    if (id) fetchPropertDetails(id);
-  }, [id]);
+  //     if (resp.status === 200) setProperty(resp.data.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // useEffect(() => {
+  // if (id) fetchPropertDetails(id);
+  // }, [id]);
 
   return (
     <Suspense>
-      <div className={`container ${classes.cardDetailWrapper}`}>
-        <div className={classes.textWrapper}>
-          <h1 className={classes.heading}>{property?.title || "N/A"}</h1>
-          <div className={classes.infoWrapper}>
-            <div className={classes.price}>
-              Price: <span>{property?.price || "N/A"}</span>
-            </div>
-            <div className={classes.ref}>
-              Ref: <span>{property?.id || "N/A"}</span>
-            </div>
-          </div>
-          <div className={classes.sliderWrapper}>
-            <SliderPro images={property?.imgs?.slice(0, 5)} />
-          </div>
-        </div>
-      </div>
+      <CardDetailComponent />
     </Suspense>
   );
 };
---
+++ src/app/components/Card/detail.js
@@ -0,0 +1,48 @@
+"use client";
+import React, { Suspense, useEffect, useState } from "react";
+import classes from "../../card-detail/cardDetail.module.css";
+import SliderPro from "../../components/Slider/index";
+import { useSearchParams } from "next/navigation";
+import { propertyService } from "@/services/property.service";
+
+const CardDetailComponent = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id"); // Get the property ID from the query string
+  const [property, setProperty] = useState({});
+
+  const fetchPropertDetails = async (id) => {
+    try {
+      const resp = await propertyService.getPropertyDetail(id);
+
+      if (resp.status === 200) setProperty(resp.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (id) fetchPropertDetails(id);
+  }, [id]);
+
+  return (
+    <Suspense>
+      <div className={`container ${classes.cardDetailWrapper}`}>
+        <div className={classes.textWrapper}>
+          <h1 className={classes.heading}>{property?.title || "N/A"}</h1>
+          <div className={classes.infoWrapper}>
+            <div className={classes.price}>
+              Price: <span>{property?.price || "N/A"}</span>
+            </div>
+            <div className={classes.ref}>
+              Ref: <span>{property?.id || "N/A"}</span>
+            </div>
+          </div>
+          <div className={classes.sliderWrapper}>
+            <SliderPro images={property?.imgs?.slice(0, 5)} />
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  );
+};
+
+export default CardDetailComponent;
