c4853a0f54937bb8df8dea6d4d4028515a2ccf40
Abdul Rehman <abdul.rehman@creasoft.io>
2024-09-25 00:37:48 +0100

    redeploy

--- src/constant/env.constant.ts
+++ src/constant/env.constant.ts
@@ -2,5 +2,5 @@
  * API endpoint
  */
 // export const baseURL = "https://real-estate-production-3570.up.railway.app";
-// export const baseURL = "https://real-estate-ec80.onrender.com"; // Reader
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://real-estate-ec80.onrender.com"; // Reader
+// export const baseURL = "http://localhost:8080";
