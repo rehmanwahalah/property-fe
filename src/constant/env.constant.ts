220ade0b53b5eebfa9ef59641a0223c8ab4144e0
Abdul Rehman <abdul.rehman@creasoft.io>
2024-09-27 15:31:24 +0100

    replace endpoint

--- src/constant/env.constant.ts
+++ src/constant/env.constant.ts
@@ -1,6 +1,6 @@
 /**
  * API endpoint
  */
-// export const baseURL = "https://real-estate-production-3570.up.railway.app";
-export const baseURL = "https://real-estate-ec80.onrender.com"; // Reader
+// export const baseURL = "https://real-estate-ec80.onrender.com"; // Reader
 // export const baseURL = "http://localhost:8080";
+export const baseURL = "https://bardd-node-pt.hf.space"; // docket endpoint
