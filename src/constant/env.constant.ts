e5bd2999f3902e657310506bb01bce72d8dcacdf
Abdul Rehman <abdul.rehman@creasoft.io>
2024-10-03 12:06:30 +0100

    redeploy

--- src/constant/env.constant.ts
+++ src/constant/env.constant.ts
@@ -3,4 +3,5 @@
  */
 // export const baseURL = "https://real-estate-ec80.onrender.com"; // Reader
 // export const baseURL = "http://localhost:8080";
-export const baseURL = "https://bardd-node-pt.hf.space"; // docket endpoint
+// export const baseURL = "https://bardd-node-pt.hf.space"; // docket endpoint
+export const baseURL = "https://r3n3r9r4-8080.usw2.devtunnels.ms"; // kaggle endpoint
